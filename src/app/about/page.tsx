637b0ee02f0a327baaaa19b21a1086ec170b4132
Anung Ramadhan <anungrmdhn@gmail.com>
2024-02-06 14:17:36 +0700

    create not-found page and bug fix

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,27 @@
 export default function About() {
-    return <div>
-        <h1>About</h1>
+  return (
+    <div>
+      <h1 className="font-bold text-2xl">About</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut vero natus
+        in fugiat porro debitis vel quos autem odio cum optio eaque, minus
+        laudantium dolore assumenda perspiciatis possimus maiores nihil expedita
+        omnis sint repellat? Consequatur modi voluptatum animi possimus
+        necessitatibus! Voluptatibus in tenetur rem dolor illo eos, doloremque
+        obcaecati quis dolorem minima impedit similique iste, explicabo ipsum
+        nostrum aliquid itaque quae sequi voluptatum harum fugiat vero eligendi
+        quasi ratione. Quaerat laudantium delectus nulla quo, qui debitis ut.
+        Nostrum sunt quaerat nobis tempore corporis amet dicta quia ut
+        accusamus. Alias sint, esse omnis placeat debitis praesentium delectus
+        sunt facere eum eaque doloribus provident natus. Reprehenderit,
+        laudantium beatae doloribus veritatis, reiciendis laborum ipsum, nobis
+        consectetur totam culpa voluptatum incidunt neque illo deleniti mollitia
+        nihil assumenda libero. Possimus, rem nostrum rerum numquam dignissimos
+        minima, in nesciunt vero a tempora eligendi cum veniam magni? Quam et
+        possimus, minus exercitationem, in adipisci voluptate beatae sint esse
+        recusandae distinctio dolor, modi aliquid rem laborum quidem dignissimos
+        quo animi cum aliquam veritatis architecto quod incidunt. Facere, quo.
+      </p>
     </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,22 @@ export default function Home() {
       <h1 className="text-4xl font-bold mb-3">Anung Ramadhan</h1>
       <p>
         Hallo, saya seorang
-        <span className="font-bold"> Software Development</span>
+        <span className="font-bold"> Software Development</span>.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+        cupiditate accusantium praesentium eveniet illum neque aliquam aut
+        perferendis perspiciatis, minus illo laudantium quos libero dolorum
+        voluptates tempora veritatis. Inventore, facilis repellendus. Iusto
+        doloribus non magni voluptatem sit, debitis, fuga quis provident nulla
+        pariatur ipsam incidunt distinctio voluptatibus eos nobis sapiente
+        tempora. Ullam doloribus nemo aut sint explicabo veritatis quibusdam
+        voluptas.
       </p>
       <div>
-        <Link href={""}><h3 className="font-bold">Linked In</h3></Link>
+        <Link href={""}>
+          <h3 className="font-bold">Linked In</h3>
+        </Link>
       </div>
     </div>
   );
